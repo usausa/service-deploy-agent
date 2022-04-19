5a0e1e30a10bd8326886adcac3d82a86c0752173
usausa <machi.pon@gmail.com>
2022-04-19 15:08:00 +0900

    Update nullable

--- Service.Deploy.Client/Models/DeployEntry.cs
+++ Service.Deploy.Client/Models/DeployEntry.cs
@@ -1,11 +1,8 @@
 namespace Service.Deploy.Client.Models;
 
-using System.Diagnostics.CodeAnalysis;
-
 public class DeployEntry
 {
-    [AllowNull]
-    public string Name { get; set; }
+    public string Name { get; set; } = default!;
 
     public string? Url { get; set; }
 
