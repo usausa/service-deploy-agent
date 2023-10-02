e8f9a0b057529bb4698d3fdb696823dd37d9de45
usausa <machi.pon@gmail.com>
2023-10-02 12:05:23 +0900

    Update setting

--- Service.Deploy.Client/GlobalSuppressions.cs
+++ Service.Deploy.Client/GlobalSuppressions.cs
@@ -1,1 +1,2 @@
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:DoNotDirectlyAwaitATaskAnalyzer", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1861:Avoid constant arrays as arguments\r\n", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2007:Do not directly await a Task", Justification = "Ignore")]
